463341e2d53db75d0f262d130be1fed1aeb4f3a9
K Fuquay <kirsty.fuquay@gmail.com>
2025-03-11 08:03:08 -0400

    Install astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,11 @@ import icon from "astro-icon";
 
 import netlify from "@astrojs/netlify";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-  output:'server',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  output: "server",
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), icon()],
   adapter: netlify(),
 });
